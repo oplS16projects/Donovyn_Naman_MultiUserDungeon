3f88c69d1c44c274701b149340ea1cd5f715080d
draconicdon <draconicdon@gmail.com>
2016-04-28 23:50:35 -0400

    Client code improvement
    Improved off site, getting its own commit.

--- OPLprojectProgress.rkt
+++ OPLprojectProgress.rkt
@@ -1,5 +1,6 @@
 #lang racket/gui
 
+
 ; Create a dialog
 (define frame (instantiate frame% ("ChatRoom")
                 [height 150]
@@ -10,34 +11,55 @@
 (define textfield-1 
 (new text-field% [parent frame] [label "Please Enter Your Name"]))
 
-(define textfield-2
-(new text-field% [parent frame] [label "Please Enter Your Name"] ))
+(define (make-field)
+    (new text-field% [parent frame] [label "look a label"])
+  )
+
+;;(define textfield-2
+;;(new text-field% [parent frame] [label "Please Enter Your Name"] ))
+
+
+(define-values [in out] (tcp-connect "localhost" 22231));added
 
 (define user1 "")
-(define user2 "")
 (define user1message "")
-(define user2message "")
+(define msg-item "")
+
+(define gbl-text "")
 
 (define (displUser1 user1)
   (string-append user1 ", type here: "))
 
-(define (displUser2 user2)
-  (string-append user2 ", type here: "))
+(define (send-message msg);added
+  (write msg out)
+  (flush-output out)
+  )
 
+
+;;(define (displUser2 user2)
+;;  (string-append user2 ", type here: "))
 (define myButton
 (new button% [parent frame] [label "Register New Users"]
-     [callback (lambda (button event)(
+     [style (list 'border)]
+     [callback (lambda (button event)
+                 (
                  (set! user1 (send textfield-1 get-value))
-                 (set! user2 (send textfield-2 get-value))
-                 (display user1)
-                 (display user2)
                  (send textfield-1 set-label (displUser1 user1))
-                 (send textfield-2 set-label (displUser2 user2))
-                 (send textfield-1 set-value "")
-                 (send textfield-2 set-value "")
-                 ))]))
+                 (send textfield-1 set-value " ")
+                 (send frame delete-child myButton)
+                 ))]
+
+     
+     ))
 
                  
+(define (print-message msg)
+  (set! gbl-text (string-append gbl-text "\n" (car msg) ": " (cadr msg)))
+  gbl-text)
+
+
+ 
+
 
 
 ; Make a static text message in the frame
@@ -54,26 +76,28 @@
 
 (define panel2 (new vertical-panel% [parent frame]
                                      [alignment '(center center)]))
- 
-; Add Cancel and Ok buttons to the horizontal panel
+
+(define received-text
+(new text-field% [parent panel1] [label ""] [min-height 400] [min-width 500])
+)
+; Add Cancel and Ok buttons to the horizontal panelasdasdas
 (new button% [parent panel1] [label "Send!"]
      [callback (lambda (button event)
-                 (send textfield-1 set-value "")
-                 (send textfield-2 set-value "")
+                 
+                 ;(send textfield-2 set-value "")
                  (set! user1message (send textfield-1 get-value))
-                 (set! user2message (send textfield-2 get-value))
-                 ;;create textfield based on user 1 or user 2
-                 (new text-field% [parent panel2](label user2)
+                 (send textfield-1 set-value "")
+                 (set! msg-item (list user1 user1message))
+                 (send-message msg-item);added
+                 
+                 (send received-text set-value (print-message msg-item));added
                       ;(set-value user1)
-                      ))])
+                      )])
 
-(define (getVar user1message user2message user1 user2)
-  (cond
-    ((eq? user1message "")(user2))
-    ((eq? user2message "") (user1))
-    ))
 
-(new button% [parent panel1] [label "Clear"])
+(new button% [parent panel1] [label "Clear"]
+     [callback (lambda (button event)
+                 (set! gbl-text ""))])
 ;;add rollback functionality ----> clear all chat messages and reset frame
 
 
